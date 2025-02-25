ca08d81a1026179bd66baab6b786aeb84007b453
Prassadraj <prassadpr10@gmail.com>
2025-02-25 18:30:53 +0530

    chgg

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,22 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        mobile: "320px",
+        // => @media (min-width: 320px) { ... }
+
+        tablet: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        laptop: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        largeLaptop: "1440px",
+        // => @media (min-width: 1440px) { ... }
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
