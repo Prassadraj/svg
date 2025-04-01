3484e5c0a82d03e3eea55e3257b82449f5a88f7f
Prassadraj <prassadpr10@gmail.com>
2025-04-01 13:53:15 +0530

    chg

--- src/app/project/ongoing/page.jsx
+++ src/app/project/ongoing/page.jsx
@@ -29,7 +29,9 @@ function Ongoing() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("/api/data");
+        const response = await axios.get(
+          "https://tminteriors.netlify.app/api/data"
+        );
         const completedProjects = response.data.data.filter(
           (project) => project.complete == false
         );
